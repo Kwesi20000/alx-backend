adbce23208b1baa867a40f05cfcec0e3aa5cdb73
Kwesi20000 <hendrickskumi@yahoo.com>
2024-05-20 22:29:37 +0000

    hendy

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -51,7 +51,7 @@ class Server:
         """
         assert type(page) == int and type(page_size) == int,
         assert page > 0 and page_size > 0,
-        
+
         start, end = index_range(page, page_size)
         data = self.dataset()
         if start >= len(data):
