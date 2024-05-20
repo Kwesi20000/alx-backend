783d52b2db906682b712e988b8e1d02ee224a615
Kwesi20000 <hendrickskumi@yahoo.com>
2024-05-20 22:31:41 +0000

    hendy

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
-"""API Design: Pagination module"""
+"""An API Design: Pagination module"""
 import csv
+import math
 from typing import Tuple, List
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Function that returns a tuple containing the start index
-    and end index corresponding to the range of indexes to return
-    in a pagination system.
-
-    Args:
-        page (int): The current page number.
-        page_size (int): The number of items per page.
-
-    Returns:
-        Tuple[int, int]: A tuple containing the start index and end index.
+    A helper function that return a tuple of page and
+    page size
     """
     start = (page - 1) * page_size
     end = start + page_size
@@ -23,14 +16,16 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """A Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset."""
+        """A Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -40,20 +35,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
-
-        Args:
-            page (int): The current page number.
-            page_size (int): The number of items per page.
-
-        Returns:
-            List[List]: A list of rows from the dataset.
-        """
-        assert type(page) == int and type(page_size) == int,
-        assert page > 0 and page_size > 0,
-
+        """ Fucntion Retrieves a page of data"""
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset()
-        if start >= len(data):
+        if start > len(data):
             return []
         return data[start:end]
